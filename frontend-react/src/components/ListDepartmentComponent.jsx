56dd677a12e74bb09a891af81e786fa8cdabe7e8
rgsax1 <rgsa@outlook.com.br>
2024-06-26 14:09:21 -0300

    Ok

--- frontend-react/src/components/ListDepartmentComponent.jsx
+++ frontend-react/src/components/ListDepartmentComponent.jsx
@@ -1,18 +1,25 @@
 import {useEffect, useState} from "react";
-import {getAllDepartments} from "../services/DepartmentService.js";
-import {Link} from "react-router-dom";
+import {getAllDepartments, updateDepartment, deleteDepartment} from "../services/DepartmentService.js";
+import {Link, useNavigate} from "react-router-dom";
 
 const ListDepartmentComponent = () => {
 
-    const [departments, setDepartments] = useState();
+    const [departments, setDepartments] = useState([]);
+    const navigate = useNavigate();
+
     useEffect(()=>{
+        listAllDepartments()
+    }, [])
+
+    function listAllDepartments(){
         getAllDepartments().then((response) => {
             console.log(response.data)
             setDepartments((response.data))
         }).catch(error => {
             console.error(error)
         })
-    }, [])
+    }
+
 
   return (
     <div className="container">
@@ -36,7 +43,7 @@ const ListDepartmentComponent = () => {
                     <td>{department.departmentDescription}</td>
                     <td>
                         <button className='btn btn-info' onClick={() => updateDepartment(department.id)}>Update</button>
-                        <button className='btn btn-danger' onClick={() => removeDepartment(department.id)}>Delete</button>
+                        <button className='btn btn-danger' onClick={() => deleteDepartment(department.id)}>Delete</button>
                     </td>
                 </tr>)
         }
